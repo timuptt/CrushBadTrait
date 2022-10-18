de64aca797ee6fbd6f23c4876542f4a7e05b5a75
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-10-18 18:24:19 +0400

    Implement view models

---
+++ src/CrushBadTraitWebApp/CrushBadTrait.WebApp/ViewModels/CreateDayReportViewModel.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace CrushBadTrait.WebApp.ViewModels;
+
+public class CreateDayReportViewModel
+{
+    public Guid TraitId { get; set; }
+    public string TraitName { get; set; } = string.Empty;
+    public string Note { get; set; } = string.Empty;
+    public int Periodicity { get; set; }
+    public int Grade { get; set; }
+}
--- src/CrushBadTraitWebApp/CrushBadTrait.WebApp/ViewModels/TraitViewModel.cs
+++ src/CrushBadTraitWebApp/CrushBadTrait.WebApp/ViewModels/TraitViewModel.cs
@@ -4,8 +4,8 @@ public class TraitViewModel
 {
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public double AverageGrade { get; set; }
-    public List<DayReportViewModel> Days { get; set; }
+    public List<DayReportViewModel>? DayReports { get; set; }
 }
