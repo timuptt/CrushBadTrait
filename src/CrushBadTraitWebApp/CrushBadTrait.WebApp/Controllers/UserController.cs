828ebd3cacb07c549349e00d9b1495f414d02420
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-10-20 20:45:04 +0400

    Implement user controller and view

---
+++ src/CrushBadTraitWebApp/CrushBadTrait.WebApp/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+using CrushBadTrait.Core.Entities.Interfaces;
+using CrushBadTrait.WebApp.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrushBadTrait.WebApp.Controllers;
+
+public class UserController : Controller
+{
+    private readonly IUserService _userService;
+    private readonly ICurrentUserService _currentUserService;
+
+    public UserController(IUserService userService, ICurrentUserService currentUserService)
+    {
+        _userService = userService;
+        _currentUserService = currentUserService;
+    }
+
+    // GET
+    public async Task<IActionResult> Profile()
+    {
+        var profileViewModel = await _userService.GetUserProfile(_currentUserService.UserId);
+        return View(profileViewModel);
+    }
+}
---
+++ src/CrushBadTraitWebApp/CrushBadTrait.WebApp/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,8 @@
+namespace CrushBadTrait.WebApp.ViewModels;
+
+public class ProfileViewModel
+{
+    public string? Name { get; set; }
+    public DateTime StartImprovingDate { get; set; }
+    public TimeOnly EndOfDayTime { get; set; }
+}
