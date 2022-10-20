da43e65fda2aaea65a2f780aa4eeba4abbbf9428
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-10-20 17:52:36 +0400

    Move user to infrastructure

--- src/CrushBadTraitWebApp/CrushBadTrait.Application/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-namespace CrushBadTrait.Core.Entities;
-
-public class User : BaseEntity
-{
-    public string Email { get; set; }
-    public string Name { get; set; }
-    public Guid UserDetailsId { get; set; }
-    public UserDetails UserDetails { get; set; }
-}
