8817488ecdffdbc72125a93d9a7ca6f28bc29ea5
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-10-20 17:54:58 +0400

    Implement identity view models

---
+++ src/CrushBadTraitWebApp/CrushBadTrait.WebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CrushBadTrait.WebApp.ViewModels;
+
+public class RegisterViewModel
+{
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare("Password")]
+    public string ConfirmPassword { get; set; }
+    [DataType(DataType.Text)]
+    public string Name { get; set; }
+    [Required]
+    [DataType(DataType.Time)]
+    public TimeOnly EndOfDayTime { get; set; }
+}
