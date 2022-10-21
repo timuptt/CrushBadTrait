088c918c2fccd0415706c486d0cbe31518f1e168
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-10-21 22:26:57 +0400

    Update DependencyInjection.cs

--- src/CrushBadTraitWebApp/CrushBadTrait.Infrastructure/Common/DependencyInjection/DependencyInjection.cs
+++ src/CrushBadTraitWebApp/CrushBadTrait.Infrastructure/Common/DependencyInjection/DependencyInjection.cs
@@ -13,14 +13,17 @@ public static class DependencyInjection
     public static IServiceCollection ImplementPersistence(this IServiceCollection services, 
         IConfiguration configuration)
     {
+        if (configuration.GetSection("UseInMemoryDb").Value.ToLower() != "true")
+        {
+            services.AddDbContext<DbContext, CrushBadTraitDbContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString("SqlServer"),
+                        b => b.MigrationsAssembly(typeof(CrushBadTraitDbContext).Assembly.FullName)));
+            return services;
+        }
+        
         services.AddDbContext<DbContext, CrushBadTraitDbContext>(o => 
             o.UseInMemoryDatabase("Traits"));
-
-        // services.AddDbContext<CrushBadTraitDbContext>(options =>
-        //         options.UseSqlite(configuration.GetConnectionString("SQLiteConnectionString"),
-        //             b => b.MigrationsAssembly(typeof(CrushBadTraitDbContext).Assembly.FullName)), 
-        //     ServiceLifetime.Transient);
-
+            
         return services;
     }
 }
